c9b5da99713032dc62bfe070aa8ab7f0ca071b8d
Adhithya1999 <anirudhadhithya20@gmail.com>
2022-06-22 09:14:46 +0530

    added vj bday

--- Royal.java
+++ Royal.java
@@ -2,7 +2,7 @@ public class Royal {
 
  public static void main(String[] args) {
   // TODO Auto-generated method stub
-    System.out.println("SHREYA");
+    System.out.println("VIJAYS BIRTDAY");
 
  }
 
