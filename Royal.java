0989fb2dea7ec4cc4dcb29324a5eefb8c4dcc2b5
Adhithya1999 <anirudhadhithya20@gmail.com>
2022-06-22 09:08:11 +0530

    added megha

--- Royal.java
+++ Royal.java
@@ -2,7 +2,7 @@ public class Royal {
 
  public static void main(String[] args) {
   // TODO Auto-generated method stub
-    System.out.println("DHARUN MAMA");
+    System.out.println("MEGHA");
 
  }
 
