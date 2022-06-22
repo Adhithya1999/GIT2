df493d114509b2a861f8b92f4c72a9a884c2378b
Adhithya1999 <anirudhadhithya20@gmail.com>
2022-06-22 09:18:05 +0530

    added june tweny two

--- Royal.java
+++ Royal.java
@@ -2,7 +2,7 @@ public class Royal {
 
  public static void main(String[] args) {
   // TODO Auto-generated method stub
-    System.out.println("VIJAYS BIRTDAY");
+    System.out.println("june 22");
 
  }
 
